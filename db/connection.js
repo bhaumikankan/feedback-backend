171bf9455446bea87c1350c722acc52b31211fc7
Ankan Bhaumik <ankanbhaumik80@gmail.com>
2023-05-23 16:36:35 +0530

    single Batch and Student model

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
-const mongoose=require('mongoose');
- mongoose.connect('mongodb+srv://hitaiml:hitaiml@cluster0.i4aw8.mongodb.net/?retryWrites=true&w=majority');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/FeedBackSys");
 //mongoose.connect('mongodb+srv://feedback:feedback@cluster0.ufdpq.mongodb.net/?retryWrites=true&w=majority');
-const conn=mongoose.connection;
+const conn = mongoose.connection;
 
-conn.on('connected',()=>{
-    console.log('db connected');
-})
+conn.on("connected", () => {
+  console.log("db connected");
+});
 
-conn.on('error',()=>{
-    console.log('db error');
-})
+conn.on("error", () => {
+  console.log("db error");
+});
